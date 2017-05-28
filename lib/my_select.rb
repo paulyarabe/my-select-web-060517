1163bd7af729db32d3a28870f8949e30bdf75a4a
Paul Yarabe <pyarabe@mba2018.hbs.edu>
2017-05-28 03:42:28 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if collection.length == 0
+    puts "There's nothing to select!"
+  else
+    i = 0
+    array = []
+    while i < collection.length
+      if yield(collection[i])
+        array << collection[i]
+      end
+      i += 1
+    end
+    array
+  end
 end
